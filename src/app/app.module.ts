cb2a9591712fd17e43afc81723fcf5e47ce965a5
Harvin Bejarano <harvinbejarano@gmail.com>
2018-12-07 15:27:24 -0500

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ],
+	imports: [
+		BrowserModule,
+		MatSidenavModule,
+		NoopAnimationsModule,
+		MatToolbarModule,
+		MatButtonModule,
+	],
+	providers: [],
+	bootstrap: [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
